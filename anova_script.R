579c4a32fb036cfe96a9c293a12829f3f68a427a
emmears <emears@nd.edu>
2018-11-28 11:05:21 -0500

    models made

--- anova_script.R
+++ anova_script.R
@@ -7,3 +7,45 @@
 
 #load data 
 antibiotics = read.csv(file="antibiotics.csv")
+
+#plot the results using ggplot
+library(ggplot2)
+plot(antibiotics)
+
+#create likelihood functions
+ThirdMod<-function(p,x1,x2,y){
+  B0=p[1]
+  B1=p[2]
+  B2=p[3]
+  sigma=exp(p[4])
+  
+  pred=B0+B1*x1+B2*x2
+  nll=-sum(dnorm(x=y,mean=pred,sd=sigma,log=TRUE))
+  
+  return(nll)
+}
+
+secondMod<-function(p,x1,y){
+  B0=p[1]
+  B1=p[2]
+  sigma=exp(p[3])
+  
+  pred=B0+B1*x1
+  nll=-sum(dnorm(x=y,mean=pred,sd=sigma,log=TRUE))
+  
+  return(nll)
+}
+
+firstMod<-function(p,x,y){
+  B0=p[1]
+  sigma=exp(p[2])
+  
+  pred=B0
+  nll=-sum(dnorm(x=y,mean=pred,sd=sigma,log=TRUE))
+  
+  return(nll)
+}
+
+
+
+
