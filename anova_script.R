fa969d4f4abee406f743e0798f062f088bd16f37
emmears <emears@nd.edu>
2018-11-28 10:44:18 -0500

    load data

--- anova_script.R
+++ anova_script.R
@@ -2,3 +2,8 @@
 ############### ANOVA #################
 #######################################
 
+#set working directory 
+#for Emily's computer: setwd("C:/Users/Vostro 3550/Desktop/Junior 18-19/Biocomputing/Biocomp_Folder/biocomputing_StatsGroupProject")
+
+#load data 
+antibiotics = read.csv(file="antibiotics.csv")
