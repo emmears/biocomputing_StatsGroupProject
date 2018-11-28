61fd810d6b98d552eb2cd7c5feebd89a42df8a1f
Ashley Fleishman <Ashley@ashleyfhmansmbp.dhcp.nd.edu>
2018-11-28 10:46:56 -0500

    regression_script.R

---
+++ regression_script.R
@@ -0,0 +1,5 @@
+### REGRESSION SCRIPT
+rm(list=ls()) #remove global environment
+setwd("/Users/Ashley/Documents/Biocomputing_2018/biocomputing_StatsGroupProject")
+
+sugar = read.csv("sugar.txt", header = TRUE)
