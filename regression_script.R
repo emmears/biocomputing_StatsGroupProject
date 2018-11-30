1aea41256bc3e65395c2e78d33a3f1e4ec231d65
Ashley Fleishman <Ashley@ashleyfhmansmbp.dhcp.nd.edu>
2018-11-30 10:54:46 -0500

    regression_script.R

--- regression_script.R
+++ regression_script.R
@@ -2,4 +2,31 @@
 rm(list=ls()) #remove global environment
 setwd("/Users/Ashley/Documents/Biocomputing_2018/biocomputing_StatsGroupProject")
 
-sugar = read.csv("sugar.txt", header = TRUE)
+sugar = read.csv("sugar.csv", header = TRUE)
+sugar
+
+library(ggplot2)
+
+plot(sugar)
+
+nllike = function(p,x,y) {
+  B0 = p[1]
+  B1 = p[2]
+  sigma = exp(p[3])
+  
+  expected = B0 + B1*x
+  
+  nll = -sum(dnorm(x=y, mean = expected, sd = sigma, log = TRUE))
+  return (nll)
+}
+
+initialGuess = c(1,1,1)
+fit = optim(par = initialGuess, fn = nllike, x=sugar$sugar, y=sugar$growth)
+print(fit)
+
+teststat = fit$value
+df = length(fit$par)
+
+pt(teststat, df)
+
+
