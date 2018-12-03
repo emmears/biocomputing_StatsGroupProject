96d5b5a6e1a4b0bb438d9a0edbce56a4b8f77e9e
Ashley Fleishman <Ashley@ashleyfhmansmbp.dhcp.nd.edu>
2018-12-03 10:42:06 -0500

    regression_script.R

--- regression_script.R
+++ regression_script.R
@@ -7,13 +7,15 @@ sugar
 
 library(ggplot2)
 
-plot(sugar)
+plot(sugar) #scatterplot of data points
 
 nllike = function(p,x,y) {
+  #assign parameters
   B0 = p[1]
   B1 = p[2]
   sigma = exp(p[3])
   
+  #linear equation
   expected = B0 + B1*x
   
   nll = -sum(dnorm(x=y, mean = expected, sd = sigma, log = TRUE))
@@ -21,28 +23,32 @@ nllike = function(p,x,y) {
 }
 
 nllike2 = function(p,x,y) {
+  #assign intercept parameter
   B0 = p[1]
   sigma = exp(p[2])
   
+  #expected equation is just the intercept
   expected = B0
   
-  nll2 = -sum(x=y, mean = expected, sd = sigma, log = TRUE)
+  nll2 = -sum(dnorm(x=y, mean = expected, sd = sigma, log = TRUE))
   return(nll2)
 }
 
-initialGuess = c(1,1,1)
+initialGuess = c(1,1,1) #initial guess for 3 parameters
 fit.complex = optim(par = initialGuess, fn = nllike, x=sugar$sugar, y=sugar$growth)
 print(fit.complex)
 
-fit.simple = optim(par = initialGuess, fn = nllike2, x = sugar$sugar, y = sugar$growth)
+initialGuess2 = c(1,1) #intial guess fro 2 parameters
+fit.simple = optim(par = initialGuess2, fn = nllike2, x = sugar$sugar, y = sugar$growth)
 print(fit.simple)
 
-teststat = 2*(fit.simple$value - fit.complex$value)
-df = length(fit.complex$par) - length(fit.simple$par)
-
-pchisq(teststat,df, lower=F)
-
+teststat = 2*(fit.simple$value - fit.complex$value) #compute test statistic for chi-squared test
+df = length(fit.complex$par) - length(fit.simple$par) #compute degrees of freedom for chi-squared test
+#check that degrees of freedom = 1
 df
+
+pchisq(teststat,df, lower=F) #run a chi-squared test to determine likelihood
+
  
 
 
