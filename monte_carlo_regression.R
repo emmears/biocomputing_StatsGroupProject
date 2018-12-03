2303f0bdc238ca1431453722660f5b6808ed11c1
Ashley Fleishman <Ashley@ashleyfhmansmbp.dhcp.nd.edu>
2018-12-03 17:04:28 -0500

    monte_carlo_regression.R

--- monte_carlo_regression.R
+++ monte_carlo_regression.R
@@ -49,8 +49,36 @@ nllike2 = function(p,x,y) {
   #expected equation is just the intercept
   expected = B0
   
-  nll2 = -sum(dnorm(x=y, mean = expected, sd = sigma, log = TRUE))
-  return(nll2)
+  nll = -sum(dnorm(x=y, mean = expected, sd = sigma, log = TRUE))
+  return(nll)
+}
+
+nllike_anova = function(p,x,y) {
+  #assign parameters
+  B0 = p[1]
+  B1 = p[2]
+  B2 = p[3]
+  B3 = p[4]
+  B4 = p[5]
+  B5 = p[6]
+  B6 = p[7]
+  B7 = p[8]
+  sigma = exp(p[9])
+  
+  #unpack the matrix
+  x1 = matrix8[,1]
+  x2 = matrix8[,2]
+  x3 = matrix8[,3]
+  x4 = matrix8[,4]
+  x5 = matrix8[,5]
+  x6 = matrix8[,6]
+  x7 = matrix8[,7]
+  
+  #create expected equation
+  expected = B0 + B1*x1 + B2*x2 + B3*x3 + B4*x4 + B5*x5 + B6*x6 + B7*x7
+  
+  nll = -sum(dnorm(x=y, mean = expected, sd = sigma, log = TRUE))
+  return(nll)
 }
 
 sig_1_mean = mean(pval[1:10])
@@ -70,5 +98,24 @@ sig_8_mean
 sig_12_mean
 sig_16_mean
 sig_24_mean
+
+#8-LEVEL ANOVA
+
+for (j in sigmas) {
+  for(i in 1:10) {
+    error = rnorm(24,0,j)
+    y[[i]]= 10 + .4*x + error
+    df1 = data.frame(x, y[[i]])
+    sort(df1,x)
+    
+    matrix8 = matrix(0,24,7)
+    matrix8[4:6,1] = 1
+    matrix8[7:9,2] = 1
+    matrix8[10:12,3] = 1
+    matrix8[13:15,4] = 1
+    matrix8[16:18,5] = 1
+    matrix8[19:21,6] = 1
+    matrix8[22:24,7] = 1
+
   
 
