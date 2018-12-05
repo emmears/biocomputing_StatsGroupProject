1cf379e4408ab2a106abcd4839c159690e1c7aad
emmears <emears@nd.edu>
2018-12-05 16:09:23 -0500

    changes to the anova part 1 section compiled

---
+++ final_part1_script.R
@@ -0,0 +1,93 @@
+######################################
+#######PART 1 OF STATS PROJECT########
+######################################
+
+#Question 1: The ANOVA
+#A student conducted an experiment evaluating the effect of three different new antibiotics 
+#on growth of E. coli in lab cultures. Using the data in antibiotics.txt, 
+#generate a plot that summarizes the results and test for an effect of antibiotic treatments 
+#on the growth of E. coli using an ANOVA-design linear model and likelihood ratio test. 
+
+#set working directory 
+#for Emily's computer: setwd("C:/Users/Vostro 3550/Desktop/Junior 18-19/Biocomputing/Biocomp_Folder/biocomputing_StatsGroupProject")
+
+#load data 
+antibiotics = read.csv(file="antibiotics.csv")
+
+#plot the results as a bow and whisker plot using ggplot
+library(ggplot2)
+plotant = plot(antibiotics,ylab="Bacterial Growth",xlab="Treatment Type")
+plotant
+
+#To begin the ANOVA analysis, distinguish between treatments by 0,1 pairings
+#make a new column for x1 and fill with 0,1 values
+x1 = c(0) #create a vector for 0s
+antibiotics$x1 = x1 #make the new x1 column and set as all 0s
+
+x1.1 = c(1) #create a vector of 1s for the 1 values
+antibiotics[5:8,3] = x1.1 #antibiotic treatment 1 is all 1 values, leaving all else as 0s
+
+#make a new column for x2 and fill with 0,1 values
+antibiotics$x2 = x1 #sets the entire column as 0s 
+antibiotics[9:12,4] = x1.1 #antibiotic treatment 2 is all 1s in column x2, leaving all else as 0s
+
+#make a new column for x3 and fill with 0,1 values
+antibiotics$x3 = x1 #sets the entire column as 0s 
+antibiotics[13:16,5] = x1.1 #antibiotic treatment 3 will be set as 1s, leaving all else as 0s
+
+#create likelihood functions for the ANOVA model
+
+#create the most complicated model 
+ANOVAMod<-function(p,x,y){
+  #assign parameters
+  B0=p[1]
+  B1=p[2]
+  B2=p[3]
+  B3=p[4]
+  sigma=exp(p[5])
+  #unpack 
+  x1 = x$x1
+  x2 = x$x2 
+  x3 = x$x3
+  #define linear equation 
+  pred=B0+B1*x1+B2*x2+B3*x3
+  nll=-sum(dnorm(x=y,mean=pred,sd=sigma,log=TRUE))
+  
+  return(nll)
+}
+
+#create the simplest model
+NullMod<-function(p,x,y){
+  #assign parameters
+  B0=p[1]
+  sigma=exp(p[2])
+  #unpack 
+  x1 = x
+  #equation
+  pred=B0
+  nll=-sum(dnorm(x=y,mean=pred,sd=sigma,log=TRUE))
+  
+  return(nll)
+}
+
+
+# estimate parameters for each model- this is where it starts/the initial conditions
+# these estimations are based on the averages shown from the plot generated above 
+ANOVAGuess=c(20,-15,-2,8,1)#estimations for most complicated ComplexMod
+NullGuess = c(20,1) #estimations for simplest SimpleMod
+
+#create fit for each model
+fitANOVA = optim(par=ANOVAGuess,fn=ANOVAMod,x = antibiotics[,3:5],y=antibiotics$growth)
+fitNull=optim(par=NullGuess,fn=NullMod,x = antibiotics$x1,y=antibiotics$growth)
+
+# run likelihood ratio tests 
+# find statistical significance of the differences between the fit of the models
+
+teststat=2*(fitNull$value-fitANOVA$value) #determine test statistic value 
+teststat #37.90132
+df=length(fitANOVA$par)-length(fitNull$par) #determine degrees of freedom 
+df #3
+
+1-pchisq(teststat,df) #chi square test for significance 
+#returns a value of 2.96576 E -08
+#p value that is given will show the degree of significance of the difference between the NullMod and the ComplexMod for the data
