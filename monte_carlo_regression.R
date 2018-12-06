44415ecefbdc1ae0ff24e148ea07054f119b89ea
Ashley Fleishman <Ashley@Ashley-Fleishmans-MacBook-Pro.local>
2018-12-05 19:12:02 -0500

    monte_carlo_

--- monte_carlo_regression.R
+++ monte_carlo_regression.R
@@ -6,28 +6,6 @@ pval = c()
 pval2 = c()
 
 y = list()
-out = list(y)
-
-for (j in sigmas) {
-  for (i in 1:10) {
-    error = rnorm(24,0,j)
-    y[[i]]= 10 + .4*x + error
-    
-    initialGuess = c(1,1,1) #initial guess for 3 parameters
-    fit.complex = optim(par = initialGuess, fn = nllike, x=x, y=y[[i]])
-    
-    initialGuess2 = c(1,1) #intial guess fro 2 parameters
-    fit.simple = optim(par = initialGuess2, fn = nllike2, x=x ,y=y[[i]])
-    
-    teststat = 2*(fit.simple$value - fit.complex$value) #compute test statistic for chi-squared test
-    df = length(fit.complex$par) - length(fit.simple$par) #compute degrees of freedom for chi-squared test
-    
-    pval = rbind(pval, pchisq(teststat,df, lower=F))
-  }
-  out[[j]] = y
-  y = list()
-}
-
 
 nllike = function(p,x,y) {
   #assign parameters
@@ -53,6 +31,45 @@ nllike2 = function(p,x,y) {
   nll = -sum(dnorm(x=y, mean = expected, sd = sigma, log = TRUE))
   return(nll)
 }
+
+for (j in sigmas) {
+  for (i in 1:10) {
+    error = rnorm(24,0,j)
+    y[[i]]= 10 + .4*x + error
+    
+    initialGuess = c(1,1,1) #initial guess for 3 parameters
+    fit.complex = optim(par = initialGuess, fn = nllike, x=x, y=y[[i]])
+    
+    initialGuess2 = c(1,1) #intial guess fro 2 parameters
+    fit.simple = optim(par = initialGuess2, fn = nllike2, x=x ,y=y[[i]])
+    
+    teststat = 2*(fit.simple$value - fit.complex$value) #compute test statistic for chi-squared test
+    df = length(fit.complex$par) - length(fit.simple$par) #compute degrees of freedom for chi-squared test
+    
+    pval = rbind(pval, pchisq(teststat,df, lower=F))
+  }
+  y = list()
+}
+
+sig_1_mean = mean(pval[1:10])
+sig_2_mean = mean(pval[11:20])
+sig_4_mean = mean(pval[21:30])
+sig_6_mean = mean(pval[31:40])
+sig_8_mean = mean(pval[41:50])
+sig_12_mean = mean(pval[51:60])
+sig_16_mean = mean(pval[61:70])
+sig_24_mean = mean(pval[71:80])
+
+sig_1_mean
+sig_2_mean
+sig_4_mean
+sig_6_mean
+sig_8_mean
+sig_12_mean
+sig_16_mean
+sig_24_mean
+
+
 
 matrix8 = matrix(0,24,7)
 matrix8[4:6,1] = 1
@@ -91,23 +108,7 @@ nllike_anova = function(p,x,y) {
   return(nll)
 }
 
-sig_1_mean = mean(pval[1:10])
-sig_2_mean = mean(pval[11:20])
-sig_4_mean = mean(pval[21:30])
-sig_6_mean = mean(pval[31:40])
-sig_8_mean = mean(pval[41:50])
-sig_12_mean = mean(pval[51:60])
-sig_16_mean = mean(pval[61:70])
-sig_24_mean = mean(pval[71:80])
 
-sig_1_mean
-sig_2_mean
-sig_4_mean
-sig_6_mean
-sig_8_mean
-sig_12_mean
-sig_16_mean
-sig_24_mean
 
 #8-LEVEL ANOVA
 
@@ -116,10 +117,9 @@ for (j in sigmas) {
     error = rnorm(24,0,j)
     y[[i]]= 10 + .4*x + error
     
-    y[[i]]=y[order(x)]
     x=sort(x)
     
-    initialGuess = c(5,5,5,5,5,5,5,5,1) #initial guess for 9 parameters
+    initialGuess = c(1,2,3,4,5,6,7,8,9) #initial guess for 9 parameters
     fit.complex = optim(par = initialGuess, fn = nllike_anova, x=x, y=y[[i]])
   
     initialGuess2 = c(1,1) #intial guess for 2 parameters
@@ -130,7 +130,6 @@ for (j in sigmas) {
     
     pval2 = rbind(pval2, pchisq(teststat,df, lower=F))
   }
-  out[[j]] = y
   y = list()
 }
 
